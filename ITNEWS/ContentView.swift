60a2b83c19b7ec3daee76ad5efa4ebee8a2ea20a
Kirill <kirillvikhlyaev88@yandex.ru>
2022-07-10 16:18:57 +0500

    Initial Commit

---
+++ ITNEWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ITNEWS
+//
+//  Created by Кирилл on 10.07.2022.
+//  Copyright © 2022 Kirill. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
